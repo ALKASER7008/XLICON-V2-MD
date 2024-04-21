90b65512bea00f40ec353be55cd1c8eba98bc2d6
ALKASER7008 <166563424+ALKASER7008@users.noreply.github.com>
2024-04-21 17:25:04 +0300

    Update sticker-tovid.js

--- plugins/sticker-tovid.js
+++ plugins/sticker-tovid.js
@@ -1,27 +1,45 @@
-import { webp2mp4 } from '../lib/webp2mp4.js'
+import fetch from 'node-fetch'
+import { sticker } from '../lib/sticker.js'
+import uploadFile from '../lib/uploadFile.js'
+import uploadImage from '../lib/uploadImage.js'
+import { webp2png, webp2mp4} from '../lib/webp2mp4.js'
+import { Sticker, StickerTypes } from 'wa-sticker-formatter'
 import { ffmpeg } from '../lib/converter.js'
 
-let handler = async (m, { conn }) => {
-    if (!m.quoted) throw '✳️ Respond to an animated sticker'
-    let mime = m.quoted.mimetype || ''
-    if (!/webp|audio/.test(mime)) throw '✳️ Respond to an animated sticker'
-    let media = await m.quoted.download()
-    let out = Buffer.alloc(0)
-    if (/webp/.test(mime)) {
-        out = await webp2mp4(media)
-    } else if (/audio/.test(mime)) {
-        out = await ffmpeg(media, [
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let pp = await conn.profilePictureUrl(who).catch(_ => hwaifu.getRandom())
+let name = await conn.getName(who)
+let stiker = false
+    let q = m.quoted ? m.quoted : m
+    let mime = (q.msg || q).mimetype || ''
+    if (!m.quoted) throw `المرجو الاشارة للملصق المتحرك الذي تريد تحويله لفيديو`
+
+    let img = await q.download?.()
+    let stek = new Sticker(img, { pack: packname, author: author, type: StickerTypes.FULL })
+    let buffer = await stek.toBuffer()
+    let out
+      try {
+        if (/webp/g.test(mime)) out = await webp2mp4(img)
+        else if (/image/g.test(mime)) out = await uploadImage(img)
+        else if (/video/g.test(mime)) out = await uploadFile(img)
+        else if (/audio/g.test(mime)) out = await ffmpeg(media, [
             '-filter_complex', 'color',
             '-pix_fmt', 'yuv420p',
             '-crf', '51',
             '-c:a', 'copy',
             '-shortest'
         ], 'mp3', 'mp4')
-    }
-    await conn.sendFile(m.chat, out, 'tovid.mp4', '✅ sticker a video' , m)
+        if (typeof out !== 'string') out = await uploadImage(img)
+        else if (/gif/g.test(mime)) out = stek
+      } catch (e) {
+        throw eror
+      }
+     await conn.sendFile(m.chat, out, 'tovid.mp4', 'تم عملية تحويل الملصق لفيديو بنجاح \n تابعني في الانستغرام\ninstagram.com/alkaser7009' , m)
 }
-handler.help = ['tovid']
+//lo mau apa??
+handler.help = ['tovideo']
 handler.tags = ['sticker']
-handler.command = ['tovideo', 'tovid']
+handler.command = /^tovid$/i
 
 export default handler
